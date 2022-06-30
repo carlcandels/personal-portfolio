a442c98eaa3a1917620c035ec5fe245e34f8fd58
Carlo Candelaria <carlcandels24@gmail.com>
2022-07-01 03:18:26 +0900

    style: NavigationSection pretified

--- src/components/NavigationSection.vue
+++ src/components/NavigationSection.vue
@@ -25,27 +25,40 @@ export default defineComponent({
       @click="toggleMenu()"
     >
       <!-- burger icon -->
-      <MenuIcon class="h-6 w-6 transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 duration-300" :class="{ hidden: !isHidden }"/>
-      
+      <MenuIcon
+        class="h-6 w-6 transition ease-in delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        :class="{ hidden: !isHidden }"
+      />
+
       <!-- close icon -->
-      <XIcon class="h-6 w-6 transition ease-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" :class="{ hidden: isHidden }"/>
-      
+      <XIcon
+        class="h-6 w-6 transition ease-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+        :class="{ hidden: isHidden }"
+      />
     </div>
     <ul
       class="md:flex justify-center mt-2"
       id="menu"
       :class="{ hidden: isHidden }"
     >
-      <li class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black">
+      <li
+        class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black"
+      >
         <RouterLink to="/">Home</RouterLink>
       </li>
-      <li class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black">
+      <li
+        class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black"
+      >
         <RouterLink to="/what-i-do">What I've worked on</RouterLink>
       </li>
-      <li class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black">
+      <li
+        class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black"
+      >
         <RouterLink to="/what-i-do-for-fun">What I do for fun</RouterLink>
       </li>
-      <li class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black">
+      <li
+        class="mx-3 py-3 text-center border-0 border-slate-100 hover:bg-slate-100 hover:text-slate-800 rounded-full shadow-sm shadow-black"
+      >
         <RouterLink to="/about">About</RouterLink>
       </li>
     </ul>
